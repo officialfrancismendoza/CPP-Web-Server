a512ba27b9520ef5a605a27f9556b43f3fe7f26b
JC Squires <50750105+jcsq6@users.noreply.github.com>
2024-02-10 18:58:58 -0500

    Delete build directory

--- build/dep/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/dep
-# Build directory: C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/build/dep
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- build/dep/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/dep/test
-# Build directory: C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/build/dep/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(oatppAllTests "oatppAllTests")
-set_tests_properties(oatppAllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/dep/test/CMakeLists.txt;135;add_test;C:/Users/franc/OneDrive/Documents/Academic and Industry work/Career 2022-2023/oatpp/dep/test/CMakeLists.txt;0;")
