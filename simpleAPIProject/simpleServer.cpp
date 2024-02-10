08fc6f7cfc88a92d769bff20a7f74166193ce56a
officialfrancismendoza <francissamuelmendoza7@gmail.com>
2024-02-10 14:47:03 -0700

    Finished bare implementation of simpleServer.cpp

--- simpleAPIProject/simpleServer.cpp
+++ simpleAPIProject/simpleServer.cpp
@@ -3,5 +3,34 @@
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
 void run() {
-    
+    // Make router for incoming HTTP requests
+    auto router = oatpp::web::server::HttpRouter::createShared();
+
+    // Create HTTP connection handler with router
+    auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
+
+    // Create TCP connection provider
+    auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8000, oatpp::network::Address::IP_4});
+
+    // Create server which takes provided TCP connections and passes them to HTTP connection handler
+    oatpp::network::Server server(connectionProvider, connectionHandler);
+
+    // Print info about server port
+    OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
+
+    // Run Server
+    server.run();
+}
+
+int main() {
+    // Initialize the Oatpp environment
+    oatpp::base::Environment::init();
+
+    // Run Application
+    run();
+
+    // Destroy Oatpp environment
+    oatpp::base::Environment::destroy();
+
+    return 0;
 }
