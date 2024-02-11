ae512af71a3e630fdd59aa3eacd1fe5ad621f12d
officialfrancismendoza <francissamuelmendoza7@gmail.com>
2024-02-10 17:20:28 -0700

    Successful response to C++ server with new API handler

--- simpleAPIProject/simpleServer.cpp
+++ simpleAPIProject/simpleServer.cpp
@@ -2,9 +2,31 @@
 #include "oatpp/network/Server.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
+// Custom Request Handler 
+class Handler : public oatpp::web::server::HttpRequestHandler {
+public: 
+    /*
+        Shared Ptr
+            - Type of smart pointer
+            - Automatically deallocates when it needs to (once there is no more copies of the pointer)
+            - Can make as many copies of it as you want; will only delete once last copy has been destroyed
+    */
+    // Just made a general handler, for now only having one purpose
+    std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest>& request) override {
+       return ResponseFactory::createResponse(Status::CODE_200, "Successful Response. Hello World!"); 
+    }
+};
+
 void run() {
     // Make router for incoming HTTP requests
     auto router = oatpp::web::server::HttpRouter::createShared();
+
+    // Create a route for GET "/hello"
+    /* 
+        NOTE: Here, we are making a shared smart ptr that references a dynamic instance of Handler
+              This is functionally the same as allocating new memory for it if we called "new Handler"
+    */
+    router->route("GET", "/hello", std::make_shared<Handler>());
 
     // Create HTTP connection handler with router
     auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
