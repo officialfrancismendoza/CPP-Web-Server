a71e8f310ae811bb1d38fa43d5cc0cd693aff4c2
officialfrancismendoza <francissamuelmendoza7@gmail.com>
2024-02-11 18:25:35 -0700

    Added README.md instructions on how to run server

---
+++ README.md
@@ -0,0 +1,6 @@
+Steps To Run
+- cd build
+- cmake ..
+- ???
+- cd /build/simpleAPIProject/Debug
+- ./server.exe
--- simpleAPIProject/simpleServer.cpp
+++ simpleAPIProject/simpleServer.cpp
@@ -1,10 +1,43 @@
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
+
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
 #include "oatpp/network/Server.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
+#include "oatpp/core/macro/codegen.hpp"
+
+// Class and generation for DTO 
+/* Begin DTO code-generation */
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * Message Data-Transfer-Object
+ */
+class MessageDto : public oatpp::DTO {
+
+  DTO_INIT(MessageDto, DTO /* Extends */)
+
+  DTO_FIELD(Int32, statusCode);   // Status code field
+  DTO_FIELD(String, message);     // Message field
+
+};
+
+/* End DTO code-generation */
+#include OATPP_CODEGEN_END(DTO)
+
 // Custom Request Handler 
 class Handler : public oatpp::web::server::HttpRequestHandler {
+private:
+  std::shared_ptr<oatpp::data::mapping::ObjectMapper> m_objectMapper;
 public: 
+  /**
+   * Constructor with object mapper.
+   * @param objectMapper - object mapper used to serialize objects.
+   */
+  Handler(const std::shared_ptr<oatpp::data::mapping::ObjectMapper>& objectMapper)
+    : m_objectMapper(objectMapper)
+  {}
+
     /*
         Shared Ptr
             - Type of smart pointer
@@ -12,12 +45,18 @@ public:
             - Can make as many copies of it as you want; will only delete once last copy has been destroyed
     */
     // Just made a general handler, for now only having one purpose
-    std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest>& request) override {
-       return ResponseFactory::createResponse(Status::CODE_200, "Successful Response. Hello World!"); 
-    }
+  std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest>& request) override {
+    auto message = MessageDto::createShared();
+    message->statusCode = 1024;
+    message->message = "Hello DTO!";
+    return ResponseFactory::createResponse(Status::CODE_200, message, m_objectMapper);
+  }
 };
 
 void run() {
+    /* Create json object mapper */
+    auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+
     // Make router for incoming HTTP requests
     auto router = oatpp::web::server::HttpRouter::createShared();
 
@@ -26,7 +65,8 @@ void run() {
         NOTE: Here, we are making a shared smart ptr that references a dynamic instance of Handler
               This is functionally the same as allocating new memory for it if we called "new Handler"
     */
-    router->route("GET", "/hello", std::make_shared<Handler>());
+    /* Route GET - "/hello" requests to Handler */
+    router->route("GET", "/books/search/{???}", std::make_shared<Handler>(objectMapper /* json object mapper */ ));
 
     // Create HTTP connection handler with router
     auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
